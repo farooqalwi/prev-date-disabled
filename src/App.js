ec5c67e9d250a08d570af305f0bccefe3d06e415
Farooq Alwi <farooqalwi313@gmail.com>
2022-10-13 16:57:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from "react";
-import { DatePicker, Space } from "antd";
+import React, { useState } from "react";
+import { DatePicker } from "antd";
 import "antd/dist/antd.css";
 import "./App.css";
 import moment from "moment";
 
 const App = () => {
+  const [startDate, setStartDate] = useState(null);
+
   return (
     <div className="App">
-      <Space>
-        <DatePicker
-          format={"DD-MM-YYYY"}
-          disabledDate={current => {
-            return current && current < moment().startOf('day')
-          }}
-        />
+      <DatePicker
+        placeholder="Start Date"
+        format={"DD-MM-YYYY"}
+        disabledDate={current => {
+          return current && current < moment().startOf('day')
+        }}
+        onChange={date => setStartDate(date)}
+      />
 
-      </Space>
+      <DatePicker
+        placeholder="End Date"
+        disabled={startDate ? false : true}
+        format={"DD-MM-YYYY"}
+        disabledDate={current => {
+          return current && current < startDate
+        }}
+      />
     </div>
   );
 };
